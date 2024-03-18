9254fa257fa8b0765e4be178d7e39214b84ce5c7
yash suthar <ysuthar8055@gmail.com>
2024-03-18 12:28:34 +0530

    updated with basic file and updated folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+const App =()=> {
+
+  return (
+    <main className="bg-white">
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
