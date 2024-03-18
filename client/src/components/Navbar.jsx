c5a898ded98841af8ef1ac536248595220e089f8
yash suthar <ysuthar8055@gmail.com>
2024-03-18 16:51:00 +0530

    navbar complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+
+import logo from "../../public/assets/images/logo.svg";
+import menu from "../../public/assets/images/menu.svg";
+import porsche from "../../public/assets/images/porsche.webp";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="w-full bg-white py-7 flex justify-between items-center px-10 max-sm:px-5">
+      <nav className="flex w-full screen-max-width items-center">
+        <div className="flex items-center">
+          <img src={menu} className="h-6"/>
+          <span className="text-black ml-2">Menu</span>
+        </div>
+        <div className="w-full max-sm:hidden">
+            <img src={logo} className="h-3.5 mx-auto" />
+        </div>
+        <div className="w-full sm:hidden">
+            <img src={porsche} className="h-10 mx-auto" />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
